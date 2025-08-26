418b7d9ae29cc4adcb57a9784a4ce0886e809c5b
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-08-25 21:20:31 -0500

    Completed Day 6

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 6/Functions/task.py
@@ -0,0 +1,31 @@
+# defining a new function
+def my_function():
+    print("Hello World")
+    print("Bye")
+my_function()
+
+# exercises and project completed on https://reeborg.ca/index_en.html
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
